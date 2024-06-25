91a42193667909c5375af03fea9b399038c56a9a
Mirko Saugo <himynameismirko@gmail.com>
2024-06-26 00:37:29 +0200

    feat(init routing): :sparkles:

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,5 @@
+// [[...sign-in]] -> catch all route for sign-in
+
+export default function SignInPage() {
+  return <>Sign in</>;
+}
---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,5 @@
+// [[...sign-up]] -> catch all route for sign-up
+
+export default function SignUpPage() {
+  return <>Sign up</>;
+}
