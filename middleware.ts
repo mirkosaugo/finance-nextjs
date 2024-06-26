f1efca8efd49ce0ababdb23890dfbe67120933ab
Mirko Saugo <himynameismirko@gmail.com>
2024-06-26 14:51:54 +0200

    chore: Update clerkMiddleware to use the request object consistently

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,22 @@
 // CLERK
 // The clerkMiddleware helper enables authentication and is where you'll configure your protected routes. 
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextApiRequest, NextApiResponse } from "next";
+
+
 import { NextResponse } from "next/server";
-
-
 
 const isProtectedRoute = createRouteMatcher([
   '/',
   // '/forum(.*)', : all forum routes are protected
 ]);
 
-export default clerkMiddleware((auth, request) => {
-  if (isProtectedRoute(request)) {
-    return auth().protect();
-  }
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
   return NextResponse.next();
+
 });
+
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
