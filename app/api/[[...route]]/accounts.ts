abbc03d077749b5aee5cbe8e59d9368c0bcafe3f
Mirko Saugo <himynameismirko@gmail.com>
2024-06-30 19:12:03 +0200

    feat: :zap: init post account

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,9 +1,11 @@
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { zValidator } from '@hono/zod-validator';
 import { eq } from "drizzle-orm";
 import { Hono } from 'hono'
 
 import { db } from '@/db/drizzle';
-import { accounts } from '@/db/schema';
+import { accounts, insertAccountSchema } from '@/db/schema';
+
 
 
 
@@ -26,6 +28,30 @@ const app = new Hono()
 
     return c.json({ data });
   })
+  .post('/',
+    clerkMiddleware(),
+    zValidator("json", insertAccountSchema.pick({ name: true })),
+    async (c) => {
+      const auth = getAuth(c);
+
+      if (!auth?.userId) {
+        return c.json({ error: 'Unauthorized' }, 401);
+      }
+
+      const values = c.req.valid('json');
+
+      // destructuring the values return the first row in the array (the inserted row)
+      const [data] = await db.insert(accounts).values({
+        userId: auth.userId,
+        ...values,
+      }).returning();
+
+      return c.json({ data });
+    });
 
 
 export default app
+
+
+
+
--- db/schema.ts
+++ db/schema.ts
@@ -1,13 +1,17 @@
-import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
-
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { createInsertSchema } from "drizzle-zod";
 // https://neon.tech/docs/guides/drizzle-migrations
 
 
 
 export const accounts = pgTable("accounts", {
-  id: serial('id').primaryKey(),
+  id: uuid('id').defaultRandom().primaryKey(),
   plaidId: text("plaid_id"),
   name: text("name").notNull(),
   userId: text("user_id").notNull(),
   createdAt: timestamp('created_at').notNull().defaultNow(),
 });
+
+// https://orm.drizzle.team/docs/zod
+// generate Zod schemas from Drizzle ORM schemas (for post requests)
+export const insertAccountSchema = createInsertSchema(accounts);
